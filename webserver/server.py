79edbcadb43d3b4a3a8871eac2ba7faf7271ff7b
WilliamRamsey <williamdawsonramsey@gmail.com>
2023-05-24 23:53:59 -0400

    Creating datascheme for devices

--- webserver/server.py
+++ webserver/server.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 
 # Mock datastructure for now
-
 class Team():
     def __init__(self, team_id, team_name, owned_devices):
         self.team_id = team_id
@@ -24,19 +23,32 @@ class Device():
             'device_id': self.device_id
         }
 
-ramsey95 = Device(1)
-roswell = Team(1, 'Hornets', ramsey95)
-print(open('index.html', 'r').read())
+# ramsey95 = Device(1)
+# roswell = Team(1, 'Hornets', ramsey95)
+
+def query_database(database, attribute, value):
+    for key in database.keys():
+        pass
+
+
+schema = {'teams': [{'team_id': 1, 'team_name': 'Hornets', 'devices': [{'device_id': 1}, {'device_id': 2}]},
+                        {'team_id': 2, 'team_name': 'Eagles', 'devices': [{'device_id': 3}, {'device_id': 4}]}]}
+
+query_database(schema, 'team_id', 1)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return open('index.html', 'r').read()
+    return open('webserver\index.html', 'r').read()
 
-@app.route('/<name>')
-def print_name(name):
-    return f'Hello {name}'
+# @app.route('/<name>')
+# def print_name(name):
+#     return f'Hello {name}'
 
-# if __name__ == '__main__':
+@app.route('/API/team', methods=['GET', 'POST'])
+def teams():
+    if request.method == 'GET':
+        pass
+
 app.run()
